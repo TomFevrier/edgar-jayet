156f81229e50b8fdb462dcc0957fe4f4611c1408
TomFevrier <tomfevrier@gmail.com>
2018-10-30 16:54:30 +0100

    Absolute URLs

--- js/architecture.js
+++ js/architecture.js
@@ -1,8 +1,11 @@
+---
+---
+
 window.onload = function() {
 
 	var hubMenu = document.getElementsByClassName('menu-item hub')[0];
 	var hub = document.createElement('img');
-	hub.setAttribute('src', '/media/hub-bleu.png');
+	hub.setAttribute('src', '{{ site.baseurl }}/media/hub-bleu.png');
 	hub.setAttribute('id', 'hub');
 	hub.style.opacity = 0;
 	document.getElementById('circle').appendChild(hub);
